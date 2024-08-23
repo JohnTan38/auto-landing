34e5abc5b8d26a63ad5a3b61926d8bb039214b29
John <psg6868@yahoo.com>
2024-08-23 11:25:25 +0800

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       service: 'hotmail',
       auth: {
         user: process.env.user,
-        pass: process.env.pass',
+        pass: process.env.pass,
       },
     });
     await new Promise((resolve, reject) => {
