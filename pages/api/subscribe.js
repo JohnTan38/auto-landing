a7ae985381002e5d38cf5debed13f3892a62d019
John <psg6868@yahoo.com>
2024-08-23 11:00:43 +0800

    Add files via upload

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -12,22 +12,49 @@ export default async function handler(req, res) {
         pass: 'realmadrid8989',
       },
     });
- 
+    await new Promise((resolve, reject) => {
+      //verify connection configuration
+      transporter.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to accept message");
+          resolve(success);
+        }
+      });
+    });
+
+
     // Set up email data
     let mailOptions = {
       from: 'azx1818@hotmail.com',
       to: email,
-      subject: 'Automate Business Process',
+      subject: 'Email Confirmation',
       text: 'Thank you for expression of interest. We will contact you.',
     };
- 
+
+    await new Promise((resolve, reject) => {
+      // send email
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
+    
+
     try {
       // Send mail with defined transport object
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: 'Confirmation success!' });
+      //await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Subscription successful!' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Email not sent. Please try again.' });
+      res.status(500).json({ message: 'Subscription failed. Please try again later.' });
     }
   } else {
     res.setHeader('Allow', ['POST']);
